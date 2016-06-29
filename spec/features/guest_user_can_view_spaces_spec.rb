7d1cd32e021b23fd6853a81d350320308fd053a7
Jennifer <jennifersoden@hotmail.com>
2016-06-28 21:56:15 -0600

    added description

--- spec/features/guest_user_can_view_spaces_spec.rb
+++ spec/features/guest_user_can_view_spaces_spec.rb
@@ -9,8 +9,9 @@ RSpec.feature "guest user sees spaces" do
 
     expect(page).to have_content(space_one.name)
     expect(page).to have_content(space_one.price)
-
+    expect(page).to have_content(space_one.description)
     expect(page).to have_content(space_two.name)
-    expect(page).to have_content(space_one.price)  
+    expect(page).to have_content(space_one.price)
+    expect(page).to have_content(space_two.description)
   end
 end
