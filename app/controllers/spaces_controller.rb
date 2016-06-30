2e9e3ccf0747a6fcc57b9ea0956db14f453e24de
riizu <rgbatty@outlook.com>
2016-06-29 23:24:16 -0600

    Add minor changes to searching
    Change searching to include all occupancies matching or greater than the entered value.

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,7 +1,11 @@
 class SpacesController < ApplicationController
   def index
-    planet = Planet.find_by(name: params[:planet])
-    @spaces = Space.where(planet: planet, occupancy: params[:occupancy])
+    if planet = Planet.find_by(name: params[:planet])
+      @spaces = Space.where("planet_id = ? AND occupancy >= ?", planet.id, params[:occupancy].to_i)
+    else
+      flash[:warning] = "Please include a planet"
+      redirect_to root_url
+    end
   end
 
   def show
