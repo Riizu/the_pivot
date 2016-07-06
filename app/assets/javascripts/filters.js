9e1b25516aea20da7e26b93cfbdbdcaca70b7929
riizu <rgbatty@outlook.com>
2016-07-06 14:58:02 -0600

    Fix filter dropdown to account for new styling

--- app/assets/javascripts/filters.js
+++ app/assets/javascripts/filters.js
@@ -1,12 +1,11 @@
 $(document).ready(function () {
-  var $spaces = $('.one-sock');
+  var $spaces = $('.space');
 
   $('#space_filter_climates').on('change', function () {
     var currentStyle = this.value;
     $spaces.each(function (index, space) {
       $space = $(space);
-      console.log($space.data('style'));
-      if ($space.data('style') === currentStyle) {
+      if ($space.data('climate') === currentStyle) {
         $space.show();
       } else {
         $space.hide();
